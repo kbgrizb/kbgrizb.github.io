8c074b64742e433450dc0f647d49809161c52367
kbgrizb <grisbykb@hendrix.edu>
2026-02-08 16:46:16 -0600

    Poems show up

---
+++ csci340lab4/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#clicker').click(function() {
+    $.ajax({
+      url: "https://poetrydb.org/random",
+      dataType: "json",
+      success: function(results) {
+        $('#poem-title').text(results[0].title);
+        $('#poem-author').text("by " + results[0].author);
+        $('#poem').text(results[0].lines);
+      },
+      error: function(xhr, status, error) {
+        console.log(error);
+      }
+    });
+  });
+});
