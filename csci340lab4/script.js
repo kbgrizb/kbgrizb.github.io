01474c1a1fe3522c87e39a7682dcfececee2fdbe
kbgrizb <grisbykb@hendrix.edu>
2026-02-09 15:39:05 -0600

    poem+pic side by side, separated functions

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,27 +1,35 @@
-$(document).ready(function() {
-  $('#clicker').click(function() {
-    $.ajax({
-      url: "https://poetrydb.org/random",
-      dataType: "json",
-      success: function(results) {
-        $('#poem-title').text(results[0].title);
-        $('#poem-author').text("by " + results[0].author);
-        $('#poem').text(results[0].lines);
-      },
-      error: function(xhr, status, error) {
-        console.log(error);
-      }
-    });
+$(document).ready(function () {
 
-    $.ajax({
-      dataType: "json",
-      url: "https://randomfox.ca/floof/",
-      success: function(results) {
-        $('#fox').attr('src', results.image).show();
-      },
-      error: function(xhr,status,error) {
-        console.log(error);
-      }
-    });
+  $('#clicker').click(function () {
+    getPoem();
+    getFox();
   });
 });
+
+function getPoem() {
+  $.ajax({
+    url: "https://poetrydb.org/random",
+    dataType: "json",
+    success: function (results) {
+      $('#poem-title').text(results[0].title);
+      $('#poem-author').text("by " + results[0].author);
+      $('#poem').text(results[0].lines);
+    },
+    error: function (xhr, status, error) {
+      console.log(error);
+    }
+  });
+};
+
+function getFox() {
+  $.ajax({
+    dataType: "json",
+    url: "https://randomfox.ca/floof/",
+    success: function (results) {
+      $('#fox').attr('src', results.image).show();
+    },
+    error: function (xhr, status, error) {
+      console.log(error);
+    }
+  });
+};
